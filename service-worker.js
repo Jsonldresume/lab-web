468eb9f3fccedd115d604b7510961d794ad27106
Ehsan <68260235+directionXcorporation@users.noreply.github.com>
2021-02-02 00:10:09 -0500

    Recreated build
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2beb53d5273d987b446833b362a43468.js"
+  "./precache-manifest.43487c1937aaded5fa36c7070dd948a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
